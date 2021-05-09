2cb7ae46baed5baa6b745e3dd873ab81e1f13492
emily-chu <emily.megan.chu@gmail.com>
2021-05-09 01:27:28 -0700

    add training data; process google cloud nlp further

--- backend/helpers/entity-analysis.js
+++ backend/helpers/entity-analysis.js
@@ -1,7 +1,7 @@
 const language = require('@google-cloud/language');
 
 const client = new language.LanguageServiceClient();
-
+const tdata = require('../training-data')
 
 const analyze = async function(text) {  
 
@@ -21,14 +21,30 @@ const analyze = async function(text) {
 
   
   // console.log('Entities:');
-  // entities.forEach(entity => {
-  //   console.log(entity.name);
-  //   console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
-  //   if (entity.metadata && entity.metadata.wikipedia_url) {
-  //     console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}`);
-  //   }
-  // });
-  return entities;
+  let processed = {
+    skills: [],
+    companies: [],
+    other: [],
+  }
+  entities.forEach(entity => {
+    if (tdata.skills.includes(entity.name)) {
+      processed.skills.push(entity.name)
+    } else if (tdata.companies.includes(entity.name)){
+      processed.companies.push(entity.name)
+    } else if (entity.salience > 0.009) {
+      processed.other.push(entity.name)
+    }
+    // console.log(entity.name);
+    // console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
+    // if (entity.metadata && entity.metadata.wikipedia_url) {
+    //   console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}`);
+    // }
+  });
+  console.log(processed)
+  return processed;
+  // processed is an object
+  // processed.companies = []
+  // processed.languages = []
 }
 
 module.exports = {
